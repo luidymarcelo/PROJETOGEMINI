933216bad931edacd9b3eabf576a463234940949
Luidy <luidy.marcelo@outlook.pt>
2025-12-05 16:05:25 -0300

    Commit sei lá # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is behind 'origin/main' by 2 commits, and can be fast-forwarded. #   (use "git pull" to update your local branch) # # Changes to be committed: #	modified:   server.js #	modified:   views/dashboard.html #

--- Backend/services/geminiService.js
+++ Backend/services/geminiService.js
@@ -5,16 +5,29 @@ const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
 export async function gerarDocumentacao(prompt, duvida) {
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+    const config = {
+      thinkingConfig: {
+        thinkingBudget: 0, 
+      },
+    };
+
+    const model = genAI.getGenerativeModel({ 
+      model: "gemini-2.5-flash",
+      config: config
+    });
 
     let instrucoes = '';
     if (duvida) {
-      instrucoes += `\n\nO usuário tem a seguinte dúvida específica sobre o relatório gerado pela view: "${duvida}". Na resposta deve ser iniciada com um resumo do que faz o relatório. Após, um resumo das tabelas usadas ex. SA1 -> Cdastro de Cliente e etc. Após um detalhamento de cada campo com seu alias junto ao campo original ex. Código de cliente - A1_COD - COD_CLIENT junto com fórmula se tiver coloque de uma forma de fácil entendimento ao usuário final tente pular 1 linha a cada campo explicado e etc.`;
+      instrucoes += `\n\nO usuário tem a seguinte dúvida específica sobre o relatório gerado pela view: "${duvida}". Quero uma resposta o mais simples possível.`;
+    } else {
+      instrucoes += `Quero uma resposta o mais simples possível.`;
     }
+    
     instrucoes += `\n\nScript SQL da view:\n\`\`\`sql\n${prompt}\n\`\`\``;
 
     const result = await model.generateContent(instrucoes);
     return (await result.response).text();
+
   } catch (error) {
     console.error('Erro ao gerar documentação com a IA:', error.message);
     return 'Erro ao gerar documentação com a IA.';
